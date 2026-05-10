18b00ad59ab51298f9d74ab91963cfcaae4bd5ef
Lucian <lucian.radu@rasirom.ro>
2026-05-10 13:52:46 +0300

    Configured vitest in vite config

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,4 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+      reportsDirectory: "../project/coverage"
+    }
+  }
 });
